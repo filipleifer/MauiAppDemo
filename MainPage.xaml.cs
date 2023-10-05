7c1b9dbe59c4bbe2770223d344cface4df504c53
Filip Leifer <filipleifer@gmail.com>
2023-10-05 08:11:06 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiAppDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+        }
+    }
+
+}
---
+++ ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace MauiAppDemo
+{
+    public class ViewModel : BindableObject, INotifyPropertyChanged
+    {
+        public string Text { get; set; } = null;
+
+        public ViewModel()
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                Text = "Text is black but should not be";
+                OnPropertyChanged(nameof(Text));
+            });
+        }
+    }
+}
